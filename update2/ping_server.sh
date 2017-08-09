42deb0c929eb80f5b0e717f5212efb8a0d9f97e7
Tuan M. Dang <tuan.dang@edgeware.tv>
2017-08-09 19:28:49 +0700

    Support server's name in server.list

--- update2/ping_server.sh
+++ update2/ping_server.sh
@@ -1,35 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
 WORKING_DIR=`readlink -f \`dirname $0\``
 
 if ! which python >/dev/null 2>&1; then
-	echo "Python not installed"
-	exit 1
+    echo "Python not installed"
+    exit 1
 fi
 if ! which python2 >/dev/null 2>&1; then
-	echo "Python 2 not installed"
-	exit 1
+    echo "Python 2 not installed"
+    exit 1
 fi
 
 if ! python --version 2>&1 | grep -q 2; then
-	PYTHON_CMD=`which python2`
+    PYTHON_CMD=`which python2`
 else
-	PYTHON_CMD=`which python`
+    PYTHON_CMD=`which python`
 fi
 
 num=0
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	num=$((num+1))
 	ip=$(echo $line | cut -d';' -f1)
-	name=$(echo $line | cut -d';' -f2)
-	if [[ $name =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-		# Guy, you swapped name and ip, but no problem
-		tmp=$ip
-		ip=$name
-		name=$tmp
-	fi
-	LOCK_FILE="$WORKING_DIR/ping_server${ip}.lock"
-	if [[ $num -eq 1 ]] && [[ $name =~ ^[G,g]ateway.* ]]; then
+    LOCK_FILE="$WORKING_DIR/ping_server${ip}.lock"
+	if [ $num -eq 1 ] && [ $(echo $line | cut -d';' -f2) == "Gateway" ]; then
 		if [ ! -r "$LOCK_FILE" ]; then
 			echo $(date +'%Y%m%d_%H%M%S') > "$LOCK_FILE"
 			$PYTHON_CMD $WORKING_DIR/ping_server.py $line
@@ -38,7 +31,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 		fi
 		[ $ret -eq 0 ] || exit 1
 	else
-		if [ ! -r "$LOCK_FILE" ] && [ -n "$line" ] ; then
+		if [ ! -r "$LOCK_FILE" ]; then
 			eval "(echo $(date +'%Y%m%d_%H%M%S') > "$LOCK_FILE" ; $PYTHON_CMD $WORKING_DIR/ping_server.py \"$line\" ; rm -f "$LOCK_FILE") &"
 		fi
 	fi
